1a222366e60d0de4f4c7bdda95f922454b366c5f
ttuan <tuantv.nhnd@gmail.com>
2015-08-27 21:22:04 +0700

    get current shell and last command

--- GLC.py
+++ GLC.py
@@ -1,53 +1,58 @@
 #!/usr/bin/python2.7
+import os
+
 
 def config_bashrc():
-	# TODO
-	print '[+]', 'File .bashrc was updated ! Thehow saved the backup to .bashrc.bak'
-	return 0
+    import shutil
+    shutil.copy2(os.path.expanduser("~/.bashrc"), os.path.expanduser("~/.bashrc.bak"))
+    f = open(os.path.expanduser("~/.bashrc"), "a")
+    f.write('PROMPT_COMMAND="history -a;$PROMPT_COMMAND"')
+    print '[+]', 'File .bashrc was updated ! Thehow saved the backup to .bashrc.bak'
 
 
 def get_zsh_last_command():
-	# TODO	
-	return "echo Example"
+    with open(os.path.expanduser("~/.zsh_history")) as f:
+        list_commands = f.readlines()
+    last_command = list_commands[-1].split(';')[1]
+    return last_command
 
 
 def get_bash_last_command():
-	# TODO
-	return 0
+    with open(os.path.expanduser("~/.bash_history")) as f:
+        list_commands = f.readlines()
+    last_command = list_commands[-1]
+    return last_command
 
 
 def get_shell():
-	# TODO
-	# return 'zsh', 'bash'.. or 0
-	return 'zsh' # for testing
+    environ = os.environ["SHELL"]
+    shell = environ.split('/')[2]
+    return shell
 
 
 def get_last_command():
-	config_bashrc()
-	shell = get_shell()
+    shell = get_shell()
 
-	if shell == 'bash':
-		return get_bash_last_command()
-	elif shell == 'zsh':
-		return get_zsh_last_command()
-	else:
-		raise Exception, "Shell not found"
-
-	return 0
+    if shell == 'bash':
+        config_bashrc()
+        return get_bash_last_command()
+    elif shell == 'zsh':
+        return get_zsh_last_command()
+    else:
+        raise Exception, "Shell not found"
 
 
 def get_prefix(command):
-	try:
-		prefix = command.split(' ')[0]
-	except Exception, e:
-		print e
-		raise Exception, "Get prefix: error ! Let me check your last command !"
-	return prefix
+    try:
+        prefix = command.split(' ')[0]
+    except Exception, e:
+        print e
+        raise Exception, "Get prefix: error ! Let me check your last command !"
+    return prefix
 
 
 if __name__ == '__main__':
-	"""
-	For testing
-	"""
-	print get_last_command()
-	
+    """
+    For testing
+    """
+    print get_last_command()
